fff4e0604617f8806b4397a99826cb76cd2af69d
unknown <rochawagner85@gmail.com>
2024-02-12 12:30:18 -0300

    primeiro commit

---
+++ produto/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from . import models
+
+class VariacaoInline(admin.TabularInline):
+    model = models.Variacao
+    extra = 1
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao_curta',
+                     'preco_marketing', 'preco_marketing_promocional']
+    inlines = [
+        VariacaoInline
+    ]
+
+
+admin.site.register(models.Produto, ProdutoAdmin)
+admin.site.register(models.Variacao)
